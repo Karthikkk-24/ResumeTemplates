4aa308f0f42dd8bfeece42281f8420e3ea937f81
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-30 09:27:04 +0530

    Added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Resume1 from './pages/Resume1';
+import Resume2 from './pages/Resume2';
 
 function App() {
-
     return (
         <>
-            
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Resume1 />} />
+                    <Route path="/resume1" element={<Resume1 />} />
+                    <Route path="/resume2" element={<Resume2 />} />
+                </Routes>
+            </Router>
         </>
     );
 }
