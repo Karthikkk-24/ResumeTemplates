2cc2e0019e579aa21a40d5790b53d7b5515ea2f5
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-08-01 22:29:49 +0530

    Created Resume 2 Structure

---
+++ src/components/resume2/Experience.jsx
@@ -0,0 +1,56 @@
+
+export default function Experience() {
+    return (
+        <section className="mb-6">
+            <h2 className="text-2xl font-semibold mb-2">Experience</h2>
+            <div className="mb-4">
+                <h3 className="text-xl font-semibold">Software Developer</h3>
+                <p className="text-gray-600">
+                    RudraTech IT Services | November 2022 - August 2024 |
+                    Ambarnath, Maharashtra, India
+                </p>
+                <ul className="list-disc list-inside mt-2">
+                    <li>
+                        Developed a RESTful API that improved data retrieval
+                        times by 50% and supported integration with third-party
+                        applications, utilizing technologies like Node.js,
+                        Express, and MongoDB.
+                    </li>
+                    <li>
+                        Led backend development for betting and gambling games,
+                        improving platform performance and scalability.
+                    </li>
+                    <li>
+                        Built and optimized APIs in Node.js, PHP, and Express.js
+                        for seamless integration and real-time data processing.
+                    </li>
+                    <li>
+                        Developed user and admin management systems, ticketing
+                        systems, and custom reporting tools.
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 className="text-xl font-semibold">Software Developer</h3>
+                <p className="text-gray-600">
+                    NEXA SOFTWARE | February 2022 - August 2022 | Pune,
+                    Maharashtra, India
+                </p>
+                <ul className="list-disc list-inside mt-2">
+                    <li>
+                        Enhanced customer interaction by developing a backend
+                        system for feedback and administration.
+                    </li>
+                    <li>
+                        Redesigned user interfaces to boost engagement and
+                        usability.
+                    </li>
+                    <li>
+                        Implemented updates on live WordPress websites,
+                        increasing functionality.
+                    </li>
+                </ul>
+            </div>
+        </section>
+    );
+}
--- src/pages/Resume2.jsx
+++ src/pages/Resume2.jsx
@@ -1,9 +1,21 @@
-
+import DownloadButton from '../components/Download';
+import Education from '../components/resume1/Education';
+import Experience from '../components/resume2/Experience';
+import Header from '../components/resume2/Header';
+import Projects from '../components/resume2/Projects';
+import Skills from '../components/resume2/Skills';
+import Summary from '../components/resume2/Summary';
 
 export default function Resume2() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <Summary />
+            <Experience />
+            <Projects />
+            <Education />
+            <Skills />
+            <DownloadButton />
+        </div>
+    );
 }
